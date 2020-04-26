7a8b6341ebb4e24314ed0d9a7ada301ae086399c
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-26 16:19:25 -0300

    Add validation form in signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+// import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => (
   <>
-    <SignUp />
+    <SignIn />
     <GlobalStyle />
   </>
 );
