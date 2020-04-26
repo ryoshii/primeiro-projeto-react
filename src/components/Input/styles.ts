42adbbe9b0f2dac15aa88f9546c7b580829196d5
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-26 15:37:02 -0300

    Show border red in error

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -3,6 +3,7 @@ import styled, { css } from 'styled-components';
 interface ContainterProps {
   isFocused: boolean;
   isFilled: boolean;
+  isErrored: boolean;
 }
 
 export const Container = styled.div<ContainterProps>`
@@ -20,6 +21,12 @@ export const Container = styled.div<ContainterProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${(props) =>
+    props.isErrored &&
+    css`
+      border-color: #c53030;
+    `}
 
   ${(props) =>
     props.isFocused &&
