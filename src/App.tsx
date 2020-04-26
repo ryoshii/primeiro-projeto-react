541b8c6243d082f8da67ed723b92c27f5ff80bf1
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-04-26 13:50:52 -0300

    Create sigin up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 import GlobalStyle from './styles/globals';
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SignUp />
     <GlobalStyle />
   </>
 );
