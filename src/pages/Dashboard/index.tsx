9e747fe4da84be8e926d7023db1db7b49f9826dc
Rodrigo Yoshioka <rodrigo.yoshioka@gmail.com>
2020-05-27 10:58:24 -0300

    feat: Add new components to dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  NextAppointment,
+  Section,
+  Appointment,
+  Calendar,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -28,6 +41,88 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars1.githubusercontent.com/u/6109271?s=400&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+                alt="Rodrigo Yoshioka"
+              />
+
+              <strong>Rodrigo Yoshioka</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/6109271?s=400&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+                  alt="Rodrigo Yoshioka"
+                />
+
+                <strong>Rodrigo Yoshioka</strong>
+              </div>
+            </Appointment>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/6109271?s=400&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+                  alt="Rodrigo Yoshioka"
+                />
+
+                <strong>Rodrigo Yoshioka</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars1.githubusercontent.com/u/6109271?s=400&u=cc5a1635f24d48562b20fe0d68e99dbdd5149917&v=4"
+                  alt="Rodrigo Yoshioka"
+                />
+
+                <strong>Rodrigo Yoshioka</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>c</Calendar>
+      </Content>
     </Container>
   );
 };
